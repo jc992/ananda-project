3fa361be08560ea1ab925b204cf4d2a2b56a9d9c
ffmcgee <jc.992@hotmail.com>
2022-09-24 12:47:29 +0000

    add on scroll fading effect to main hero.

--- src/modules/MainHero.jsx
+++ src/modules/MainHero.jsx
@@ -1,44 +1,64 @@
-import styled from 'styled-components';
-import { Button, Grid, Typography } from '@mui/material';
-import { Container } from '@mui/system';
-import HomeIcon from '@mui/icons-material/Home';
-import heroImage from '../images/MainHero.jpeg';
+import styled from "styled-components";
+import { Button, Grid, Typography } from "@mui/material";
+import { Container } from "@mui/system";
+import HomeIcon from "@mui/icons-material/Home";
+import heroImage from "../images/MainHero.jpeg";
+import { useEffect, useState } from "react";
 
 const Wrapper = styled.div`
+  opacity: ${({ opacity }) => opacity};
   background: url(${heroImage});
   background-size: cover;
   min-height: 80vh;
 `;
 
-export const MainHero = () => (
-  <Wrapper>
-    <Container>
-      <Grid container spacing={2}>
-        <Grid item sm={6}>
-          <HomeIcon sx={{ cursor: 'pointer', fontSize: 40, color: '#fff' }} />
+export const MainHero = () => {
+  const [opacity, setOpacity] = useState(1);
+
+  const opacityOnScroll = () => {
+    const baseline = 1000;
+    const opacity = baseline - window.scrollY * 1.7;
+    if (opacity < 0) return;
+    !window ? setOpacity(1) : setOpacity(opacity / baseline);
+  };
+
+  useEffect(() => {
+    document.addEventListener("scroll", () => opacityOnScroll());
+    return () =>
+      document.removeEventListener("scroll", () => opacityOnScroll());
+  }, []);
+
+  return (
+    <Wrapper opacity={opacity}>
+      <Container>
+        <Grid container spacing={2}>
+          <Grid item sm={6}>
+            <HomeIcon sx={{ cursor: "pointer", fontSize: 40, color: "#fff" }} />
+          </Grid>
+          <Grid align="end" item sm={6}>
+            <Typography variant="h6" color="white" sx={{ cursor: "pointer" }}>
+              Get Early Access
+            </Typography>
+          </Grid>
+          <Grid item sm={8}>
+            <Typography variant="h1" color="white" sx={{ margin: "2rem 0" }}>
+              A New Way To Start Business
+            </Typography>
+          </Grid>
+          <Grid item sm={8}>
+            <Typography paragraph color="white">
+              Lorem ipsum dolor sit amet, id nec enim autem oblique, ei dico
+              mentitum duo. Illum iusto laoreet his te. Lorem partiendo mel ex.
+              Ad vitae admodum voluptatum per.
+            </Typography>
+          </Grid>
+          <Grid item sm={6}>
+            <Button variant="contained" color="primary">
+              Get Started
+            </Button>
+          </Grid>
         </Grid>
-        <Grid align="end" item sm={6}>
-          <Typography variant="h6" color="white" sx={{ cursor: 'pointer' }}>
-            Get Early Access
-          </Typography>
-        </Grid>
-        <Grid item sm={8}>
-          <Typography variant="h1" color="white" sx={{ margin: '2rem 0' }}>
-            A New Way To Start Business
-          </Typography>
-        </Grid>
-        <Grid item sm={8}>
-          <Typography paragraph color="white">
-            Lorem ipsum dolor sit amet, id nec enim autem oblique, ei dico mentitum duo. Illum iusto laoreet his te.
-            Lorem partiendo mel ex. Ad vitae admodum voluptatum per.
-          </Typography>
-        </Grid>
-        <Grid item sm={6}>
-          <Button variant="contained" color="primary">
-            Get Started
-          </Button>
-        </Grid>
-      </Grid>
-    </Container>
-  </Wrapper>
-);
+      </Container>
+    </Wrapper>
+  );
+};
